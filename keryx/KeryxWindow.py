5d330f6b3e170a1157269f6edff1fed9fa2cb0d2
Chris Oliver <excid3@gmail.com>
2011-10-04 23:43:56 -0500

    Create profiles

--- keryx/KeryxWindow.py
+++ keryx/KeryxWindow.py
@@ -3,6 +3,11 @@
 # This file is in the public domain
 ### END LICENSE
 
+import os
+import platform
+import shutil
+import traceback
+
 import gettext
 from gettext import gettext as _
 gettext.textdomain('keryx')
@@ -14,11 +19,17 @@ logger = logging.getLogger('keryx')
 from keryx_lib import Window
 from keryx.AboutKeryxDialog import AboutKeryxDialog
 from keryx.PreferencesKeryxDialog import PreferencesKeryxDialog
+from keryx.MessageDialogs import *
+
+EXT = ".keryx"
+SUPPORTED = ["Ubuntu"]
+PATHS = [os.path.abspath("data"),
+         os.path.abspath(os.path.expanduser("~/keryx"))]
 
 # See keryx_lib.Window.py for more details about how this class works
 class KeryxWindow(Window):
     __gtype_name__ = "KeryxWindow"
-    
+
     def finish_initializing(self, builder): # pylint: disable=E1002
         """Set up the main window"""
         super(KeryxWindow, self).finish_initializing(builder)
@@ -27,4 +38,89 @@ class KeryxWindow(Window):
         self.PreferencesDialog = PreferencesKeryxDialog
 
         # Code for other initialization actions should be added here.
+        self._initialize_home()
 
+
+    def on_add_profile_button_clicked(self, widget, data=None):
+        # Find our output directory
+        path_used = self._find_path()
+
+        # Make sure the output directory is writable
+        if not path_used:
+            error_dialog(self, "Make sure one of the following paths are writable: %s" % ", ".join(PATHS))
+            return
+
+        # Retrieve the profile name
+        profile_name = self.ui.profile_name_entry.get_text()
+        profile_dir = os.path.join(path_used, "profiles", profile_name)
+        profile_path = os.path.join(profile_dir, "%s.keryx" % profile_name)
+
+        # Create the output directory if necessary
+        if not os.path.exists(profile_dir):
+            os.makedirs(profile_dir)
+
+        if os.path.exists(profile_path):
+            error_dialog(self, "A profile with this name already exists!")
+            return
+
+        try:
+            self._write_keryx_file(profile_path)
+            self._copy_sources(profile_dir)
+
+            # Alert that patient user the good news, we've got a project written
+            # for them! :D
+            info_dialog(self, "Your computer has been added successfully!\n" \
+                    "Take this profile to an online machine to download" \
+                    "packages.\n\n" \
+                    "You can find the profile here:\n%s" % profile_dir)
+        except Exception, e:
+            traceback.print_exc()
+            error_dialog(self, "There was a problem creating your profile: \n" \
+                    "%s" % e)
+
+
+    def _write_keryx_file(self, profile_path):
+        # Write out the profile attributes
+        # - Distro (Ubuntu)
+        # - Version (11.04)
+        # - Architecture (x86_64/i386)
+        f = open(profile_path, "wb")
+        f.write("\n".join(platform.uname()[0:1]))
+        f.close()
+
+    def _copy_sources(self, profile_dir):
+        # Copy the sources
+        shutil.copy("/etc/apt/sources.list", profile_dir)
+        shutil.copytree("/etc/apt/sources.list.d", os.path.join(profile_dir,
+            "sources.list.d"))
+
+    def _copy_status(self, profile_dir):
+        # Copy dpkg status
+        shutil.copy("/var/lib/dpkg/status", profile_dir)
+
+
+    def _initialize_home(self):
+        """Initialize anything in the home screen"""
+        self.ui.profile_name_entry.set_text(platform.uname()[1])
+        self._load_profiles()
+
+        # Hide the add computer expander if running an unsupported OS
+        if platform.uname()[0] != "Linux" or not platform.dist()[0] in SUPPORTED:
+            self.ui.add_expander.set_expanded(False)
+
+
+    def _load_profiles(self):
+        """Append all the .keryx profiles found into the list"""
+        for path in PATHS:
+            for (path, dirs, files) in os.walk(os.path.join(path, "profiles")):
+                for f in files:
+                    if f.endswith(EXT):
+                        self.ui.computers_liststore.append((f.strip(EXT),os.path.join(path,f)))
+
+
+    def _find_path(self):
+        """Finds a path to write the profile to"""
+        for path in PATHS:
+            if os.access(os.path.abspath(path), os.W_OK):
+                return path
+        return None
---
+++ keryx/MessageDialogs.py
@@ -0,0 +1,70 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2010 Chris Oliver <chris@excid3.com>
+#                    mac9416 <mac9416@keryxproject.org>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+"""We have here several simple dialogs for convenience."""
+
+import gtk
+
+def dialog(parent, text, title, buttons, gtk_message):
+    """A generic dialog box"""
+
+    md = gtk.MessageDialog(parent,
+                           gtk.DIALOG_DESTROY_WITH_PARENT,
+                           gtk_message,
+                           buttons,
+                           text)
+
+    if title: md.set_title(title)
+    result = md.run()
+    md.destroy()
+    return result
+
+
+def error_dialog(parent, text, title=None, buttons=gtk.BUTTONS_OK):
+    """
+        An error dialog box
+        Returns True if OK response
+    """
+    result = dialog(parent, text, title, buttons, gtk.MESSAGE_ERROR)
+    return result == gtk.RESPONSE_OK
+
+
+def info_dialog(parent, text, title=None, buttons=gtk.BUTTONS_OK):
+    """
+        An information dialog box
+        Returns True if OK response
+    """
+    result = dialog(parent, text, title, buttons, gtk.MESSAGE_INFO)
+    return result == gtk.RESPONSE_OK
+
+
+def question_dialog(parent, text, title=None, buttons=gtk.BUTTONS_YES_NO):
+    """
+        A Yes/No question dialog box
+        Returns True if Yes response
+    """
+    result = dialog(parent, text, title, buttons, gtk.MESSAGE_QUESTION)
+    return result == gtk.RESPONSE_YES
+
+
+def warning_dialog(parent, text, title=None, buttons=gtk.BUTTONS_OK):
+    """
+        A warning dialog box
+        Returns True if OK response
+    """
+    result = dialog(parent, text, title, buttons, gtk.MESSAGE_WARNING)
+    return result == gtk.RESPONSE_OK
